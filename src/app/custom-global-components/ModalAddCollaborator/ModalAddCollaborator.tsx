e4f9e42560e0fbe44571fb3d44e64a32b65c6023
John The Bryan <johnbryanjavellana@gmail.com>
2026-03-17 10:23:45 +0800

    123

--- src/app/custom-global-components/ModalAddCollaborator/ModalAddCollaborator.tsx
+++ src/app/custom-global-components/ModalAddCollaborator/ModalAddCollaborator.tsx
@@ -9,6 +9,7 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { Autocomplete, Box, TextField } from "@mui/material";
 import CustomAvatarWithOnlineBadge from "../CustomAvatarWithOnlineBadge/CustomAvatarWithOnlineBadge";
+import LoadingPopup from "../LoadingPopup/LoadingPopup";
 
 interface ModalAddCollaboratorProps {
     data: any | null,
@@ -110,6 +111,8 @@ export default function ModalAddCollaborator({ data, src, type, id, titleHeader,
 
     return (
         <>
+            {isSubmitting && <LoadingPopup />}
+
             <ModalTemplate
                 id={`add_collaborator_${id}`}
                 size={collaborator ? "xl" : 'md'}
@@ -182,7 +185,7 @@ export default function ModalAddCollaborator({ data, src, type, id, titleHeader,
                             Close
                         </button>
 
-                        <button type="button" onClick={() => AddFutureCollaborator()} disabled={!collaborator || isFetching} className={`btn btn-danger btn-sm elevation-1`}>
+                        <button type="button" onClick={() => AddFutureCollaborator()} disabled={!collaborator || isFetching} className={`btn btn-danger btn-sm elevation-1 custom-border-dark custom-bg-maroon text-white`}>
                             Add
                         </button>
                     </div>
