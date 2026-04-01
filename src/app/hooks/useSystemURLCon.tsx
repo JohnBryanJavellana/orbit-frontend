70e03ed44f1a09bb0bf93685a7cef149988590a4
John The Bryan <johnbryanjavellana@gmail.com>
2026-04-01 11:12:45 +0800

    123

--- src/app/hooks/useSystemURLCon.tsx
+++ src/app/hooks/useSystemURLCon.tsx
@@ -1,6 +1,6 @@
 export default function useSystemURLCon() {
-    const urlWithApi = 'http://192.168.255.127:8000/api';
-    const urlWithoutApi = 'http://192.168.255.127:8000';
+    const urlWithApi = 'http://192.168.255.127:8001/api';
+    const urlWithoutApi = 'http://192.168.255.127:8001';
 
     return { urlWithApi, urlWithoutApi };
 }
